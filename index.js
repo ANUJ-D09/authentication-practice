1049f9fb59e3fa51c9314c8ebe30a40115a319d8
anujda09 <anujdamani9@gmail.com>
2025-06-05 22:12:27 +0530

    todaysda

--- index.js
+++ index.js
@@ -6,10 +6,21 @@ app.use(express.json());
 
 const users = [];
 
-function genratetoken() {
-    return Math.random();
+function generateToken() {
+    let options = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6'
+    ];
 
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+
+    return token;
 }
+
 app.post('/signup', function(req, res) {
     const userName = req.body.username;
     const passWord = req.body.passWord;
@@ -22,7 +33,9 @@ app.post('/signup', function(req, res) {
 });
 
 app.post('/signin', function(req, res) {
-    res.json("signin route");
+    const userName = req.body.username;
+    const passWord = req.body.passWord;
+
 });
 
 app.listen(port, () => {
